7314dd4c99affbc2164291e53df167bbf4cf92e6
coinevo <coinevo12@gmail.com>
2020-07-28 22:07:07 +0200

    evo

--- src/networks.js
+++ src/networks.js
@@ -1,18 +1,18 @@
 module.exports = {
     qtum: {
-        messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'qc',
+        messagePrefix: '\x15COinevo Signed Message:\n',
+        bech32: 'ec',
         bip32: {
             public: 0x0488b21e,
             private: 0x0488ade4
         },
-        pubKeyHash: 0x3a,
-        scriptHash: 0x32,
+        pubKeyHash: 0x00,
+        scriptHash: 0x05,
         wif: 0x80
     },
     qtum_testnet: {
-        messagePrefix: '\x15Qtum Signed Message:\n',
-        bech32: 'tq',
+        messagePrefix: '\x15Evo Signed Message:\n',
+        bech32: 'te',
         bip32: {
             public: 0x043587cf,
             private: 0x04358394
